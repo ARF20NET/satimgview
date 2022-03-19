ae5748e89cf4bb7de403d855ce4a5ff7fe7492bc
arf20 <aruizfernandez05@gmail.com>
2022-03-19 14:15:08 +0100

    Adapt to raspberry-noaa-v2 imagery filenames and structure

--- index.php
+++ index.php
@@ -60,33 +60,63 @@ $type = $_GET['type'];
 		<a class="text" href="?type=raw">raw</a>
 		<a class="text" href="?type=HVC">HVC</a>
 		<a class="text" href="?type=HVC-precip">HVC-precip</a>
-		<a class="text" href="?type=HVCT-precip">HVCT-precip</a>
 		<a class="text" href="?type=HVCT">HVCT</a>
-		<a class="text" href="?type=MCIR-precip">MCIR-precip</a>
+		<a class="text" href="?type=HVCT-precip">HVCT-precip</a>
 		<a class="text" href="?type=MCIR">MCIR</a>
-		<a class="text" href="?type=MSA-precip">MSA-precip</a>
+		<a class="text" href="?type=MCIR-precip">MCIR-precip</a>
 		<a class="text" href="?type=MSA">MSA</a>
+		<a class="text" href="?type=MSA-precip">MSA-precip</a>
 		<a class="text" href="?type=therm">therm</a>
+		<a class="text" href="?type=ZA">ZA</a>
+		<a class="text" href="?type=CC">CC</a>
+		<a class="text" href="?type=HE">HE</a>
+		<a class="text" href="?type=HF">HF</a>
+		<a class="text" href="?type=MD">MD</a>
+		<a class="text" href="?type=BD">BD</a>
+		<a class="text" href="?type=MB">MB</a>
+		<a class="text" href="?type=JF">JF</a>
+		<a class="text" href="?type=JJ">JJ</a>
+		<a class="text" href="?type=LC">LC</a>
+		<a class="text" href="?type=TA">TA</a>
+		<a class="text" href="?type=WV">WV</a>
+		<a class="text" href="?type=NO">NO</a>
+		<a class="text" href="?type=sea">sea</a>
 		<hr>
 		<div class="content">
 
 		<?php
+			$imgdir = "/d/FTPServer/satimg/apt/";
 			
+			$files = array_filter(scandir($imgdir), function($item) {
+				global $imgdir;
+				return !is_dir($imgdir.$item);
+			});
 			
-			$imagdir = "/d/FTPServer/satimg/apt/".$type."/";
-			$files = scandir($imagdir);
+			$files = array_filter(scandir($imgdir), function($item) {
+				global $type;
+				return substr(substr($item, 24), 0, -4) == $type;
+			});
 			
+			$files = array_values($files);
+			
+			//print_r($files);
 			
 			$times = array();
+			
 			foreach ($files as $file)
-				if ($file != "." && $file != "..")
-					array_push($times, substr($file, 8, 16));			
+				array_push($times, substr($file, 8, 15));
+			
 			rsort($times);
+			//array_reverse($times);
+			
+			//print_r($times);
 			
 			foreach ($times as $time) {
 				$file = array_search_partial($files, $time);
 				
-				$satstr = substr($file, 0, strpos($file, '_'));
+				//print($file);
+				
+				$satstr = substr($file, 0, 7);
 
 				$timestr = substr($time, 0, 4)."-".substr($time, 4, 2)."-".substr($time, 6, 2)." ".substr($time, 9, 2).":".substr($time, 11, 2).":".substr($time, 13, 2);
 				$timestamp = strtotime($timestr);
@@ -94,7 +124,7 @@ $type = $_GET['type'];
 				$ago = time() - $timestamp;
 				$agostr = secsToAgo($ago);
 				
-				echo "<a href=\"/FTPServer/satimg/apt/$type/$file\"><img width=\"100%\" src=\"/FTPServer/satimg/apt/$type/$file\"></a><br>
+				echo "<a href=\"/FTPServer/satimg/apt/$file\"><img width=\"100%\" src=\"/FTPServer/satimg/apt/$file\"></a><br>
 					<div class=\"text\"<span>$satstr: $timestr, $agostr ago</span></div><br>";
 			}
 		?>
